979a84fedf0de21463063921237faefbbd5d1573
Alessandro Gelsi <79928745+Alegelx24@users.noreply.github.com>
2023-06-18 23:49:04 +0200

    feat: now all working lesgo

--- speech_to_text.py
+++ speech_to_text.py
@@ -4,7 +4,10 @@ import pyttsx3
 import langid
 import langdetect
 import threading
+import sys
 from PIL import Image
+from playsound import playsound
+
 
 #interfaccia
 import tkinter as tk
@@ -13,11 +16,22 @@ from tkinter import Tk, Button, Label
 from PIL import ImageTk, Image
 from tkinter.filedialog import askopenfilename
 
-########################################################
-#                                                      #
-#               FUNZIONI          GENERICHE            #
-#                                                      #
-# ######################################################
+pause_event = threading.Event()
+stop_event = threading.Event()
+task_thread = None
+
+button_style = {
+    'font': ('Arial', 8),
+    'bg': 'grey',
+    'fg': 'white',
+    'activebackground': 'blue',
+    'activeforeground': 'white',
+    'relief': 'raised',
+    'borderwidth': 2,
+    'width': 10,
+    'height': 2,
+}
+
             
 #funzione per aprire un immagine
 def open_image(pathname):
@@ -33,27 +47,30 @@ def SpeakText(command):
     engine.runAndWait()
 
 
-########################################################
-#                                                      #
-#       INIZIO THREAD PRINCIPALE AUDIO RECOGNITION     #
-#                                                      #
-# ######################################################
-
+#funzione audio recognizer
 def recognizer(window):
     r = sr.Recognizer()
 
-    while(1):   #infinite loop to iteratively look for new audio
-           
+    while not stop_event.is_set():
+
+        while  pause_event.is_set():
+            # Wait when the pause event is set
+            pass   
+
         with sr.Microphone() as source2: #si sceglie qua il microfono come input
-                
+
+            print("I'm listeningg")   
+            window.title("Listening")
+ 
             r.adjust_for_ambient_noise(source2, duration=0.2) #sensibilita microfono
                 
             audio2 = r.listen(source2) 
         
-        # Speech recognition in Italian
         try: #per gestire gli errori se qualcosa va storto
 
+           # MyText=r.recognize_google(audio2) #prende audio e lo converte in testo, presupponendo sia italiano
             MyText=r.recognize_google(audio2, language='it-IT') #prende audio e lo converte in testo, presupponendo sia italiano
+
             MyText = MyText.lower() #mette testo minuscolo
             print("You said: " + MyText) #appare sul terminale il testo
             
@@ -61,53 +78,105 @@ def recognizer(window):
             
             language2= langdetect.detect(MyText) #altra funzione di un altra libreria per la lingua
 
-            # Print the detected language
+            # Stampa detected language
             print("Detected languages:", language )
             print("Detected languages numero 2:", language )
 
-            if(language=="it"):
-                #SpeakText("Buongiorno caro amico, ti auguro una buona giornata!")
-                # Open the image file
-                image = Image.open("./test.jpg") #qua ci va il path dell'immagine da mostrare
-                # Show the image
-                # image.show()
-                
-                photo_image= open_image("./test.jpg")
+            #inserire qui le varie immagini associate alla lingua
+            if language == "it":
+                image_path = "./test.jpg"
+            elif language == "es" or language == "eo":
+                image_path = "./test.jpg"
+            else:
+                # Lingua non riconosciuta, usa l'immagine predefinita
+                image_path = "./test2.jpg"
 
-                # Crea una label per visualizzare l'immagine
-                label1 = tk.Label(image=photo_image)
-                label1.image = photo_image
-
-                label1.place(x=0, y=0)
-      
-            elif(language == "es" or language == "eo"):
-                #SpeakText("Hola amigooooooo como to estas")
-                image = Image.open("./test.jpg")
-
-
+            image = Image.open(image_path)
+            photo = ImageTk.PhotoImage(image)
+            label1.configure(image=photo)
+            label1.image = photo
 
         except sr.UnknownValueError:
             print("Sorry, I could not understand what you said, the language is not in my dictionary")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
                                         
-  
-########################################################
-#                                                      #
-#       INIZIO THREAD VIDEO                            #
-#                                                      #
-# ######################################################
+
+#################------------FUNCTIONS--------#################################
+
+def start_listening():
+    global task_thread
+    pause_event.clear()
+    stop_event.clear()
+    task_thread = threading.Thread(target=recognizer, args=(window,))
+    task_thread.start()
+   
+def pause_listening():
+    window.title("Paused")
+    pause_event.set()
+
+def resume_listening():
+    pause_event.clear()
+
+def stop_listening():
+    global task_thread
+    if task_thread and task_thread.is_alive():
+        stop_event.set()
+        task_thread.join()
+        stop_event.clear()
+        sys.exit()  
+
+def play_audio(filename):
+    playsound(filename)  
+
+#Funzione che gestisce il click sullo schermo per far partire la voce
+def handle_click(event):
+    x = event.x
+    y = event.y
+    #finestra di pixel nella quale deve avvenire il click ->
+    if 100 <= x <= 1000 and 100 <= y <= 1000:
+        print("Clicked on the specified portion of the screen!")
+        #play_audio("./nomefile.mp3")
+        SpeakText("Buongiorno caro amico, ti auguro una buona giornata!") #qui leggo la storia
+
+
+
+
+
+
+
+#################------------MAIN--------#################################
 
 if __name__ =="__main__":
-    # Crea una finestra
+        # Crea una finestra
         window = tk.Tk()
-        window.geometry("1920x1080")
+        window.geometry("1080x1920") #formato finestra
         window.resizable(True, True)
+        window.state('zoomed')
+        window.title("Title for my program") #Inserire il titolo del progetto !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                
+        photo_image = open_image("./sfondo.jpg")  # Immagine predefinita di sfondo
+        label1 = tk.Label(window)
+        label1.configure(image=photo_image)
+        label1.place(x=0, y=0) #coordinate pixel per immagine di sfondo
 
-        # Create a separate thread to perform the task
-        task_thread = threading.Thread(target=recognizer, args=(window,))
-        task_thread.start()
+        # Create buttons for control
+        start_button = tk.Button(window, text="Start", command=start_listening, **button_style)
+        start_button.pack
+        start_button.place(x=600, y=750) #coordinate button start
 
+        pause_button = tk.Button(window, text="Pause", command=pause_listening, **button_style)
+        pause_button.pack
+        pause_button.place(x=700, y=750) #coordinate button pause
+
+        resume_button = tk.Button(window, text="Resume", command=resume_listening, **button_style)
+        resume_button.pack
+        #resume_button.place(x=800, y=750)
+
+        window.bind("<Button-1>", handle_click)
+
+        #stop_button = tk.Button(window, text="Stop", command=stop_listening)
+       # stop_button.place(x=400, y=450)
 
         # Start the Tkinter event loop
         window.mainloop()
